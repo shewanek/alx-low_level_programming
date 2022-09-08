da429448fa4d1aad88510329481a2cf8401217af
Shewanek <shewanehzewdu@gmail.com>
2022-09-08 21:57:33 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,7 @@
  * Return: Always 0 (Successful)
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
