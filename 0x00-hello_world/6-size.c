2b4bcfeae41fa2344a57f908efa6a5bd5899fd3d
Shewanek <shewanehzewdu@gmail.com>
2022-09-08 23:04:29 +0300

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
+char a;
+int b;
+long int c;
+long long int d;
 float e;
 printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(b));
