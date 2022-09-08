ee7e9bb31c681a02ce4c622102cf08afa26b7144
Shewanek <shewanehzewdu@gmail.com>
2022-09-08 21:25:29 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main - Entry point
@@ -6,7 +5,6 @@
  * Return: Always 0 (Successful)
  */
 int main(void)
-{ 
-    puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
