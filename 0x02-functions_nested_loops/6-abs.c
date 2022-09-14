3ab7d40edaaa35a07ce7093427e0b94eee7510e4
Shewanek <shewanehzewdu@gmail.com>
2022-09-14 23:47:58 +0300

    Task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _abs - computes the absolute value of an integer 
- * @n: number 
+ * _abs - computes the absolute value of an integer
+ * @n: number
  *
- * Return: absolute valueof n 
+ * Return: absolute valueof n
  */
 int _abs(int n)
 {
