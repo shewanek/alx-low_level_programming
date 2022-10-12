a953d80459c50132e1570cc4b319111b153a6725
Shewanek Zewdu <shz.cbo@gmail.com>
2022-10-12 13:49:55 +0300

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int m;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (m = 0; m < size; m++)
+	{
+		if (cmp(array[m]))
+			return (m);
+	}
+	return (-1);
+}
