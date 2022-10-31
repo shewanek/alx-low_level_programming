d2a48353198168ab2c9fedfa5af5676b5034551e
Shewanek Zewdu <shz.cbo@gmail.com>
2022-10-31 15:03:43 +0300

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,19 +11,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, l = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (l = 0; text_content[l];)
-			l++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, l);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
@@ -31,3 +31,4 @@ int append_text_to_file(const char *filename, char *text_content)
 	close(o);
 
 	return (1);
+    
